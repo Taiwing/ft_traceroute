a566d7491c91bab02e2955555a74586d2811d81f
taiwing <yforeau@student.42.fr>
2021-09-08 00:17:54 +0200

    Handle options and destination

--- includes/ft_traceroute.h
+++ includes/ft_traceroute.h
@@ -6,7 +6,7 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/06 23:40:29 by yforeau           #+#    #+#             */
-/*   Updated: 2021/09/06 23:58:19 by yforeau          ###   ########.fr       */
+/*   Updated: 2021/09/08 00:17:03 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,28 @@
 
 # include "libft.h"
 
+/*
+** t_trcrt_config: traceroute configuration
+**
+** exec: executable name
+** dest: destination argument
+** max_ttl: max number of hops
+*/
 typedef struct	s_trcrt_config
 {
-	char		*exec;
+	const char	*exec;
+	const char	*dest;
+	int			max_ttl;
 }				t_trcrt_config;
+
+/*
+** traceroute macros
+*/
+# define	MAX_TTL_DEF			30
+# define	FT_TRACEROUTE_OPT	"hm:"
+# define	FT_TRACEROUTE_HELP	"Usage:\n\t%s [options] <destination>\n"\
+	"Options:\n\t<destination>\t\thostname or IPv4 address\n"\
+	"\t-h\t\t\tprint help and exit\n"\
+	"\t-m max_ttl\t\tset max number of hops (max TTL)\n"
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,18 +6,62 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/06 23:38:42 by yforeau           #+#    #+#             */
-/*   Updated: 2021/09/07 00:00:32 by yforeau          ###   ########.fr       */
+/*   Updated: 2021/09/08 00:16:05 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_traceroute.h"
 
-int	main(int argc, char **argv)
+void		intopt(int *dest, t_optdata *optd, int min, int max)
+{
+	int		ret;
+	char	*err;
+
+	if ((ret = ft_secatoi(dest, min, max, optd->optarg)))
+	{
+		if (ret == FT_E_NOT_A_NUMBER)
+			ft_asprintf(&err, "invalid argument: '%s'", optd->optarg);
+		else
+			ft_asprintf(&err, "invalid argument: '%s': "
+				"out of range: %d <= value <= %d", optd->optarg, min, max);
+		ft_exit(err, EXIT_FAILURE);
+	}
+}
+
+static char	*get_options(t_trcrt_config *cfg, int argc, char **argv)
+{
+	int			opt;
+	t_optdata	optd;
+	char		**args;
+
+	ft_bzero((void *)&optd, sizeof(t_optdata));
+	init_getopt(&optd, FT_TRACEROUTE_OPT, NULL, NULL);
+	args = ft_memalloc((argc + 1) * sizeof(char *));
+	ft_memcpy((void *)args, (void *)argv, argc * sizeof(char *));
+	*args = (char *)cfg->exec;
+	while ((opt = ft_getopt(argc, args, &optd)) >= 0 || argc == 1)
+		switch (opt)
+		{
+			case 'm': intopt(&cfg->max_ttl, &optd, 0, 255);	break;
+			default:
+				ft_printf(FT_TRACEROUTE_HELP, cfg->exec);
+				ft_exit(NULL, opt != 'h');
+		}
+	ft_memdel((void **)&args);
+	return (argv[optd.optind]);
+}
+
+int			main(int argc, char **argv)
 {
 	t_trcrt_config	cfg = { 0 };
 
 	(void)argc;
 	cfg.exec = ft_exec_name(*argv);
+	ft_exitmsg((char *)cfg.exec);
+	cfg.max_ttl = MAX_TTL_DEF;
+	cfg.dest = get_options(&cfg, argc, argv);
 	ft_printf("This is %s!\n", cfg.exec);
+	ft_printf("traceroute to %s (%s), %d hops max, 60 byte packets\n",
+		cfg.dest, "IP_PLACEHOLDER", cfg.max_ttl);
 	return (EXIT_SUCCESS);
 }
